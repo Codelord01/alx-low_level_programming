abc46b59eea1f3a5d5cc8ed161b5a4148cfa2b60
Codelord01 <davidawuniababio@gmail.com>
2022-06-20 23:13:08 +0000

    substring check

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+/**
+ * _strspn - finds the lenght of a prefix substring
+ * @s: main string
+ * @accept: substring
+ * Return: returns an int to show the not substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
+
