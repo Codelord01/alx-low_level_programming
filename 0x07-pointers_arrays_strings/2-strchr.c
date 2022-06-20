50d14e1050d83c93d80a3c25263a9cad9e00be60
Codelord01 <davidawuniababio@gmail.com>
2022-06-20 20:36:10 +0000

    used to search for a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to be searched
+ * @c: the character to be loacated
+ * Return: returns a pointer to the first occurrence c in the string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != c && *(s + i) != c)
+	{
+		i++;
+	}
+
+	if (*(s + i) == c)
+	{
+		return (*(s + i));
+	}
+	else
+	{
+	return (0);
+	}
+}
