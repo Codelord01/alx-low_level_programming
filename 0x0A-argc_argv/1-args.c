5f86e759cc90709199b3a4c31f2621e8abae9cb3
Codelord01 <davidawuniababio@gmail.com>
2022-06-25 15:34:22 +0000

    prints the number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
+
 /**
  * main - where the function runs
  * @argc:  counts the number of arguments
  * @argv:  stores the arguments
  * Return:  returns an integer 0
- */
+**/
 
 int main(int argc, char *argv[])
 {
 (void)argv;
 
-printf("%d \n", argc - 1);
+printf("%d \n", (argc - 1));
+
 return (0);
 }
+
