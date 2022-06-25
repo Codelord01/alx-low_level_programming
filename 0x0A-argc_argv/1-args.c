1016b5283e03cff281f94af449c70d833470ec65
Codelord01 <davidawuniababio@gmail.com>
2022-06-25 15:38:29 +0000

    prints the number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 
 /**
- * main - where the function runs
- * @argc:  counts the number of arguments
- * @argv:  stores the arguments
- * Return:  returns an integer 0
+ * main - prints the for the program.
+ * @argc: is a counter of argument for int.
+ * @argv: is a counter for value of the argument to char
+ * Return: Returns always success.
 **/
 
 int main(int argc, char *argv[])
 {
 (void)argv;
 
-printf("%d \n", (argc - 1));
+printf("%d\n", (argc - 1));
 
 return (0);
 }
-
