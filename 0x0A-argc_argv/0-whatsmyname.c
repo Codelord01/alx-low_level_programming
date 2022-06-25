45e10fe45f97f7131a6d3f20da03276392e59c2d
Codelord01 <davidawuniababio@gmail.com>
2022-06-25 15:03:27 +0000

    a code that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - this is main function where everything is executed
+ * @argc: this stores the number of arguments passed
+ * @argv: this stores the arrays being passed
+ * Return: returns and integer 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s \n", argv[0]);
+	return (0);
+}
