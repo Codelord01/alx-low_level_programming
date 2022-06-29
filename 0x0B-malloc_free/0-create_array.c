70e8f7c40c7c1e0b4bacd83b46d7b51c28a97890
Codelord01 <davidawuniababio@gmail.com>
2022-06-29 18:00:01 +0000

    This is the readme to this repository

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+/**
+ * create_array - create an array and initializes with a specific char
+ * @size: stores the size of the array
+ * @c: stores the character
+ * Return: returns a pointer or null when it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+}
