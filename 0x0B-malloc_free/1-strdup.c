851e561ad5bd1f82a743eb7fe7129640a88c6ee0
Codelord01 <davidawuniababio@gmail.com>
2022-06-29 18:38:41 +0000

    create a new memory location and copies a string into it

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * _strdup - creates a new memory space and puts a copy of a string
+ * @str: the string to be kept in the memory
+ * Return: returns a pointer to the new location of the string
+ */
+
+char *_strdup(char *str)
+{
+	char *new_loc = malloc(sizeof(str));
+	unsigned int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	if (new_loc == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	while (str[i] != '\0')
+	{
+		new_loc[i] = str[i];
+		i++;
+	}
+	}
+	return (new_loc);
+}
