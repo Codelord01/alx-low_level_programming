37b095379adaa5d66ac3549498b4ab9810924c38
Codelord01 <davidawuniababio@gmail.com>
2022-07-01 22:29:26 +0000

    dynamic allocation of 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * alloc_grid - allocates memory to a two dimentional array
+ * @width: stores the number of columns in the 2d array
+ * @height: stores teh number of rows in the 2d array
+ * Return: Returns an integer array
+ **/
+
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(int *) * (height));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+			return (NULL);
+
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
