091412c298b77c89a352acf7facd37b89b121b66
Codelord01 <davidawuniababio@gmail.com>
2022-07-02 23:09:18 +0000

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+/**
+ * check_null - checks to see where one or all of arguments are null
+ * @s1: stores the first string
+ * @s2: stores the second string
+ * Return: Returns a pointer to the concatenated string
+ */
+
+char *check_null(char *s1, char *s2)
+{
+	unsigned int i = 0, j = 0;
+	char *new;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	else if (s1 != NULL && s2 == NULL)
+	{
+		new = malloc(sizeof(char) * sizeof(s1 - 1));
+		if (new == NULL)
+			return (NULL);
+		while (s1[i] != '\0')
+		{
+			new[i] = s1[i];
+			i++;
+		}
+		return (new);
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+		new = malloc(sizeof(char) * n);
+		if (new == NULL)
+			return (NULL);
+		while (s[j] != '\0')
+		{
+			new[j] = s2[j];
+			j++;
+		}
+	}
+	return (new);
+}
+
+
 /**
  * str_concat - concatenates two string and put it into a new pointer
  * @s1: stores the value of the first string
@@ -12,9 +56,9 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *new_string;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 	{
-		return (NULL);
+		check_null(s1, s2);
 	}
 
 	while (s1[len1] != '\0')
@@ -26,7 +70,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		len2++;
 	}
-	new_string = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (new_string == NULL)
 	{
