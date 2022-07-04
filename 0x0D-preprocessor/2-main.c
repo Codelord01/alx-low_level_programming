89834d616aeeaa2a180309a4d2394baed8a9b9d6
Codelord01 <davidawuniababio@gmail.com>
2022-07-04 12:51:47 +0000

    macro that finds file location

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - the main function where the code is executed
+ * Return: returns 0 on success
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
