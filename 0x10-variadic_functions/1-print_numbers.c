df8b86f28ce0147008fc361b5bf0cb75860411f3
Codelord01 <davidawuniababio@gmail.com>
2022-07-12 19:01:52 +0000

    prints the arguments and saparates them with comma

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include<stdarg.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: string that separates the numbers
+ * @n: number of arguments
+ * Return: returns nothing
+ **/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int value;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, int);
+		printf("%d%s", value, separator);
+	}
+	printf("\n");
+	va_end(args);
+}
